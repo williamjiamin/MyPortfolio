865d69a9ac66f1e75a423506e96e6a63a188e9cf
williamjiamin <lipai_wang@163.com>
2022-04-12 20:31:46 +0800

    init commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
         animation: 'fade',
         play: 3000,
         pagination: false
+    });
 
-    })
+    var typed = new Typed(".typed", {
+        strings: ["Innovator", "Investor", "Entrepreneur", "Musician", "Digital Artist"],
+        typeSpeed: 100,
+        loop: true,
+        startDelay: 200,
+        showCursor: false
+    });
+
+
 })
